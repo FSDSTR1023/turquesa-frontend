2a94ad68f1d7e4a2b38fe3d21deacb3b06d71415
Juan Mencía <juamm99@gmail.com>
2024-01-17 19:54:01 +0100

    Pagina principal creada

--- src/components/MuestraTarjeta.jsx
+++ src/components/MuestraTarjeta.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 
-const MuestraTarjeta = () => {
+const MuestraTarjeta = (tarjeta) => {
     return (
         <div className="muestraTarjetaContenedor">
             <img src="https://images.unsplash.com/photo-1488628075628-e876f502d67a?dpr=1&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=&bg=" alt="" />
