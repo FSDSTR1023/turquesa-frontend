a91b7231908c13a8464a728ff9e4e74c7449a289
Carmen Plaza <carmenplazaromero@gmail.com>
2024-02-07 20:24:42 +0100

    cambio en axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const baseURL = import.meta.env.VITE_BASE_URL;
 const instance = axios.create({
-    baseURL: baseURL,
+    baseURL: 'http://localhost:3000',
     withCredentials:true,
 });
 
