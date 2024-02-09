f2d4cc4276b1ff2b4dc6e8ceff90ef55168438df
m-marinucci <mmarinucci@numinate.com>
2024-02-09 22:05:26 +0100

    completado astente.api.js

---
+++ src/api/asistente.api.js
@@ -0,0 +1,8 @@
+import axios from './axios'; // Assuming axios.js configures the base URL and other settings
+
+// Function to get all asistentes
+export const getAsistentes = () => axios.get('/asistentes');
+
+// Function to create a new asistente
+export const crearAsistente = (asistente) => axios.post('/asistentes/create', asistente);
+
