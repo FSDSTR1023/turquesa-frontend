1870c7799c6af5840352d14c20bf45bf22d141e0
Carmen Plaza <carmenplazaromero@gmail.com>
2024-02-23 13:42:18 +0100

    Cambios y correciones- menu navbar interfería css de otros componentes- corregido- maquetación botones fijos- otras correcciones de css copiados por error

--- src/pages/TarjetaDelUsuario.jsx
+++ src/pages/TarjetaDelUsuario.jsx
@@ -1,16 +1,18 @@
 import { useTarjeta } from '../components/TarjetaContexto';
 import { Link } from "react-router-dom";
+import '../styles/PaginaPrincipal.css';
+
 
 const TarjetaDelUsuario = () => {
     const {tarjetaUsuario, compruebaTarjeta} = useTarjeta();
 
     return (
-        <div>
+        <div >
             {compruebaTarjeta(tarjetaUsuario)}
-
-            <Link to="/adquirida/edit">Editar tarjeta</Link>
-            <Link to="/gestion-invitados">Enviar invitaciones</Link>
-        
+            <div className='colocarBotones BotonesTarjetadelUsuario'>
+            <Link className='buttomMorado' to="/adquirida/edit">Editar tarjeta</Link>
+            <Link className='buttomMorado' to="/gestion-invitados">Enviar invitaciones</Link>
+            </div>
         </div>
     );
 }
