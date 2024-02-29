7a473840a1259b77e15f21828031cc9e9b577483
m-marinucci <mmarinucci@numinate.com>
2024-03-01 00:16:02 +0100

    corrected axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 // const baseURL = import.meta.env.VITE_BASE_URL;
 const instance = axios.create({
     // baseURL: baseURL,
-    baseURL:"https://api.render.com/deploy/srv-cnfqopv109ks738kb7a0?key=hK3DRevrcXU",
+    baseURL:"https://turquesa-backend.onrender.com",
     withCredentials:true,
 });
 
