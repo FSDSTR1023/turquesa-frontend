a324e0d134194bc52087e66adbed496bbb16d803
m-marinucci <mmarinucci@numinate.com>
2024-03-07 19:01:18 +0100

    comment

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
- //const baseURL = import.meta.env.VITE_BASE_URL;
+
 const instance = axios.create({
     // baseURL: baseURL,
     baseURL:import.meta.env.VITE_INVITO_BACKEND_URL,
