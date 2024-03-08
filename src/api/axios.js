6f56562020bd6a23597ab6ae2b4f3e0e6fcc3ab8
Juan Mencía <juamm99@gmail.com>
2024-03-08 12:11:59 +0100

    Changes to deploy

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
  //const baseURL = import.meta.env.VITE_BASE_URL;
 const instance = axios.create({
     // baseURL: baseURL,
-    baseURL:import.meta.env.VITE_BASE_URL,
+    baseURL:import.meta.env.VITE_INVITO_BACKEND_URL,
     withCredentials:true,
 });
 
